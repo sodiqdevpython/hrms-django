4bc2a7aa605ad529765ab965f37aa75e53955667
Sharif Orzikulov <orzikulov@gmail.com>
2024-02-29 12:13:48 +0300

    Changes

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -15,8 +15,8 @@ class Employee(models.Model):
     #STAFF_CHOICES = [("WhiteCollar", "Белый воротник"), ("BlueCollar", "Синий воротник")]
 
     current_status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default="active", verbose_name="Статус"
-    )
+        max_length=10, choices=STATUS_CHOICES, default="active", verbose_name="Статус")
+    
     personnel_number = models.CharField(max_length=200, unique=True, verbose_name="Табельный номер")
     surname = models.CharField(max_length=200, verbose_name="Фамилия")
     firstname = models.CharField(max_length=200, verbose_name="Имя")
@@ -24,30 +24,30 @@ class Employee(models.Model):
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default="male", verbose_name="Пол")
     date_of_birth = models.DateField(default=timezone.now, verbose_name="Дата рождения")
     position = models.CharField(max_length=200, verbose_name="Должность")
+
     """ current_class = models.ForeignKey(
         StudentClass, on_delete=models.SET_NULL, blank=True, null=True
     ) """
+
     citizenship = models.ForeignKey(
-        Citizenship, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Гражданство"
-    )
+        Citizenship, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Гражданство")
 
     current_doc_category = models.ForeignKey(
-        PermitDocCategory, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Категория документа"
-    )
+        PermitDocCategory, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Категория документа")
+    
     date_of_employment = models.DateField(default=timezone.now, verbose_name="Дата приема")
 
-    date_of_dismissal = models.DateField(verbose_name="Дата увольнение")
+    date_of_dismissal = models.DateField(blank=True, null=True, verbose_name="Дата увольнение")
 
     tin_number = models.CharField(max_length=200, blank=True, verbose_name="ИНН")
 
     snils_number = models.CharField(max_length=200, blank=True, verbose_name="СНИЛС")
 
     mobile_num_regex = RegexValidator(
-        regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!"
-    )
+        regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!")
+    
     mobile_number = models.CharField(
-        validators=[mobile_num_regex], max_length=13, blank=True, verbose_name="Тел номер"
-    )
+        validators=[mobile_num_regex], max_length=13, blank=True, verbose_name="Тел номер")
 
     address = models.TextField(blank=True, verbose_name="Адрес в РФ")
     others = models.TextField(blank=True, verbose_name="Другие")
